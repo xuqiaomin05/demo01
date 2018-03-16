d7a76d3c6b53b18b902d71dea2d59299a21dde3c
xu min <dumiao@51xianqu.net>
2018-03-16 19:21:11 +0800

    提交测试代码

---
+++ src/test/java/Test.java
@@ -0,0 +1,29 @@
+/**
+ * Created by xumin on 18/3/16.
+ */
+public class Test {
+    @org.junit.Test
+    public void test_01_测试1() throws Exception{
+        System.out.println("1111");
+    }
+    @org.junit.Test
+    public void test_02_测试2() throws Exception{
+        System.out.println("2222");
+    }
+    @org.junit.Test
+    public void test_03_测试3() throws Exception{
+        System.out.println("3333");
+    }
+    @org.junit.Test
+    public void test_04_测试4() throws Exception{
+        System.out.println("4444");
+    }
+    @org.junit.Test
+    public void test_05_测试5() throws Exception{
+        System.out.println("5555");
+    }
+    @org.junit.Test
+    public void test_06_测试6() throws Exception{
+        System.out.println("6666");
+    }
+}
