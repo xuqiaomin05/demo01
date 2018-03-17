c97bf3951610a93f7d0d4c930922ddb9d6e7ee26
xuqiaomin05 <xuqiaomin05@163.com>
2018-03-17 16:43:39 +0800

    修改脚本

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -24,6 +24,7 @@ public class Test {
     }
     @org.junit.Test
     public void test_06_测试6() throws Exception{
-        System.out.println("6666");
+        System.out.println("6666新增的");
     }
+
 }
